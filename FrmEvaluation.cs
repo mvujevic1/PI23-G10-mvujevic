16433267caf3ff50ab8d543a51d49031e60cedcb
Marko Vujević <mvujevic@foi.hr>
2023-05-16 16:22:38 +0200

    Dovrsena cetvrta

--- FrmEvaluation.cs
+++ FrmEvaluation.cs
@@ -15,6 +15,8 @@ namespace Evaluation_Manager
     public partial class FrmEvaluation : Form
     {
         private Student student;
+
+        public Student SelectedStudent { get => student; set => student = value; }
         public FrmEvaluation(Student selectedStudent)
         {
             InitializeComponent();
@@ -41,10 +43,35 @@ namespace Evaluation_Manager
             txtMinForSignature.Text = currentActivity.MinPointsForSignature + "/" + currentActivity.MaxPoints;
             numPoints.Minimum = 0;
             numPoints.Maximum = currentActivity.MaxPoints;
+
+            var evaulation = EvaulationRepository.GetEvaluation(SelectedStudent, currentActivity);
+            if (evaulation != null)
+            {
+                txtTeacher.Text = evaulation.Evaluator.ToString();
+                txtEvaluationDate.Text = evaulation.EvaulationDate.ToString();
+                numPoints.Value = evaulation.Points;
+
+            }
+            else
+            {
+                txtTeacher.Text = FrmLogin.LoggedTeacher.ToString();
+                txtEvaluationDate.Text = "-";
+                numPoints.Value = 0;
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
+            Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var activity =cboActivities.SelectedItem as Activity;
+            var teacher = FrmLogin.LoggedTeacher;
+            int points = (int)numPoints.Value;
+
+            teacher.PerformEvaluation(SelectedStudent, activity, points);
             Close();
         }
     }
--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Evaluation_Manager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,24 @@ namespace Evaluation_Manager.Models
         public string Username { get; set; }
         public string Password { get; set; }
 
+        public bool CheckPassword(string password)
+        {
+            return Password == password;
+        }
+
+        public void PerformEvaluation(Student student, Activity activity, int points)
+        {
+            var evaulation = EvaulationRepository.GetEvaluation(student, activity);
+            if (evaulation == null)
+            {
+                EvaulationRepository.InsertEvaluation(student, activity, this, points);
+
+            }
+            else
+            {
+                EvaulationRepository.UpdateEvaluation(evaulation, this, points);
+            }
+
+        }
     }
 }
